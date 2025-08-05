550456b673434be8826f09babe856ef38b5bdf69
nikiiigoyal <nikiiigoyal@gmail.com>
2025-08-05 09:40:43 +0530

    Update in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 const App = () => {
-  return <h1 className='text-2xl font-bold'>Search Github Users</h1>;
+  const [userName, setUserName] = useState('quincylarson');
+  return (
+    <div className='flex  items-center justify-center h-screen'>
+      <div className='flex gap-4'>
+        <Button>Click me</Button>
+        <Button variant='outline' size='lg'>
+          Click me
+        </Button>
+        <Button variant='destructive' size='sm'>
+          Click me
+        </Button>
+      </div>
+    </div>
+  );
 };
 export default App;
