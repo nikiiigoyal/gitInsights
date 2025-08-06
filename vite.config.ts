a8059bc2a915a389b653c1f4c033c85dedc8ec71
nikiiigoyal <nikiiigoyal@gmail.com>
2025-08-06 16:56:28 +0530

    Modify tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname,
+         './src'),
     },
   },
 });
