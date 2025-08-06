28050385ae7de4abeb3d24e475c04b06de6a6ebc
nikiiigoyal <nikiiigoyal@gmail.com>
2025-08-06 16:55:05 +0530

    Add new component form

---
+++ src/components/user/UserProfile.tsx
@@ -0,0 +1,11 @@
+type UserProfileProps = {
+    userName: string;
+   
+}
+
+const UserProfile = ({userName}: UserProfileProps) => {
+ return (
+   <h1 className="text-2xl font-bold">{userName}</h1>
+ )
+}
+export default UserProfile;
