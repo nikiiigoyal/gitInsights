0d930d960ef36c59109e3e77fcfcd3dff01b6949
nikiiigoyal <nikiiigoyal@gmail.com>
2025-08-08 09:22:11 +0530

    Update in package

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { ApolloProvider } from '@apollo/client'
 import client from './apolloClient.ts'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
     <App />
+    <Toaster />
   </ApolloProvider>,
 )
